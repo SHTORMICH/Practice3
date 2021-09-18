628fd6915893db0f26ed791c8d43489424210a41
Olexandr <olesukmail@gmail.com>
2021-09-18 16:36:16 +0300

    Part1 updates

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -1,24 +1,90 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class Part1 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        StringBuilder textInFile = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("part1.txt"))) {
+            bufferedReader.readLine();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                textInFile.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String text = textInFile.toString();
+
+        System.out.println(text);
+
+        System.out.println("\nConvert 1");
+        System.out.println(convert1(text));
+        System.out.println("\nConvert 2");
+        System.out.println(convert2(text));
+        //System.out.println("\nConvert 3");
+        //System.out.println(convert3(text));
+        //System.out.println("\nConvert 4");
+        //System.out.println(convert4(text));
     }
 
     public static String convert1(String input) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        String[] sentence = input.split("\n");
+        for (String str : sentence) {
+            String[] words = str.split(";");
+            result.append(words[0]).append(": ").append(words[2]).append("\n");
+        }
+        return result.toString();
     }
 
     public static String convert2(String input) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        String[] sentence = input.split("\n");
+        for (String str : sentence) {
+            String[] words = str.split(";");
+            for (String word: words) {
+                System.out.println(word);
+            }
+            result.append(words[1]).append(" (email: ").append(words[2]).append(")\n");
+        }
+        return result.toString();
     }
 
     public static String convert3(String input) {
-        return null;
+        StringBuilder resultMail = new StringBuilder();
+        StringBuilder resultGog = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        String[] sentence = input.split("\n");
+        for (String str : sentence) {
+            String[] words = str.split(";");
+            String word = words[2].split("@")[1];
+            if (word.equals("mail.com")) {
+                resultMail.append(words[0]).append(", ");
+            } else {
+                resultGog.append(words[0]).append(", ");
+            }
+        }
+        result.append("mail.com ==> ")
+                .append(resultMail.substring(0, resultMail.length() - 2))
+                .append("\n")
+                .append("google.com ==> ")
+                .append(resultGog.substring(0, resultGog.length() - 2));
+        return result.toString();
     }
 
     public static String convert4(String input) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        String[] sentence = input.split("\n");
+        Random random = new Random();
+        for (String str : sentence) {
+            result.append(str).append(";").append(random.nextInt(9000) + 1000).append("\n");
+        }
+        return result.toString();
     }
 }
